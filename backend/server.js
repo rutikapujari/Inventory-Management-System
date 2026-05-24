93b3a2d08a56b013601199f5777d3fbc310a5350
ShagunMishra640 <shagunmishra640@gmail.com>
2026-05-24 15:50:00 +0530

    merged to main branch

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const app = require("./app");
+const app = require("./app.js");
 const dashboardRoutes = require("./src/modules/admin/routes/dashboardRoutes");
 const adminRoutes = require("./src/modules/admin/routes/adminRoutes");
 const analyticsRoutes = require("./src/modules/admin/routes/analyticsRoutes");
